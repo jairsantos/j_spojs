deff68ea5a19804fe2e52982b65ae80281440846
Jair Santos <jairdesouzasantos@gmail.com>
2013-10-28 20:33:54 -0200

    Salvando atualizações

--- c/wcw.c
+++ c/wcw.c
@@ -1,22 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Esse é uma estrutura que será usada para guardar os elementos e
-// a contagem dos elementos;
+// Esse é uma estrutura que será usada para guardar instâncias e
+// a contagem das intâncias;
 struct instance {
-	// Guarda o número total de elementos.
+	// Guarda o número de elementos.
 	int elements_count;
 
-	// Guarda os elementos em si.
+	// Ponteiro que aponta para os elementos
 	int *elements;
+
+	// Essa variável guarda o número de movimentos necessários
+	// para ordenar o vetor.
+	int sort_count = 0;
+};
+
+struct s_root {
+	// Guarda o número de instâncias
+	int instances_count;
+
+	// Ponteiro para as intâncias
+	struct instance *instances;
 };
 
 // Função que realiza a troca entre os valores
-void swap(int a, int b)
+void swap(int *a, int *b)
 {
-	int x = a;
-	a = b;
-	b = a;
+	int x = *a;
+	*a = *b;
+	*b = x;
 }
 
 // Essa é a função responsável por ordenar o vetor.
@@ -28,11 +40,11 @@ int linear_sort(int ary[], int size)
 
 	for(i = 0; i < size; i++)
 	{
-		for(j = 0; j < size; j++)
+		for(j = i; j < size; j++)
 		{
-			if (ary[i] < ary[j])
+			if (ary[j] < ary[i])
 			{
-				swap(ary[i], ary[j]);
+				swap(&ary[i], &ary[j]);
 				count++;
 			}
 		}
@@ -45,56 +57,50 @@ void main()
 
 	// Esses serão os ponteiros responsáveis por armazenar as
 	// instâncias, e os elementos dessas instâncias.
-	struct instance *instances;
+	struct s_root root;
 
 	// Variáveis para fim de controle de loops no código.
 	int i, j;
 
-	// Essa variável armazenará a quantidade de instâncias que
-	// será disponibilizada pelo usuário.
-	int instances_quantity = 0;
-
-	// Essa variável armazenará a quantidade de elementos na
-	// estrutura de dados.
-	int sequence_quantity  = 0;
 
 	// Requisita a quantidade de instâncias para o usuário.
-	scanf("%d", &instances_quantity);
+	scanf("%d", &root.instances_count);
 
 	// Aloca a quantidade de memória necessária para fazer a leitura
 	// das instâncias dadas pelo usuário.
-	instances = (struct instance*) malloc (instances_quantity + 1);
+	root.instances = (struct instance*) malloc (root.instances_count + 1);
 
 	// Faz a interação para a leitura da quantidade de instâncias pedidas.
-	for(i = 0; i < instances_quantity; i++)
+	for(i = 0; i < root.instances_count; i++)
 	{
 		// Requisita a quantidade de elementos do vetor que será
 		// lido.
-		scanf("%d", &sequence_quantity);
+		scanf("%d", &root.instances[i].elements_count);
 
 		// Aloca a quantidade de memória necessária para fazer a leitura
 		// dos elementos dados pelo usuário.
-		instances[i].elements = (int*) malloc (sequence_quantity + 1);
+		root.instances[i].elements = (int*) malloc (root.instances[i].elements_count + 1);
 
 		// Faz a iteração para a leitura da sequência dada pelo usuário.
 		//-------------------------------
-		for(j = 0; j < sequence_quantity; j++)
+		for(j = 0; j < root.instances[i].elements_count; j++)
 		{
 			// Requisita o elemento para o usuário, e armazena no espaço
 			// indicado pelo ponteiro.
-			scanf("%d", &instances[i].elements[j]);
+			scanf("%d", &root.instances[i].elements[j]);
 		}
 		//--------------------------------
 	}
 
-	for(i = 0; i < instances_quantity; i++)
+	// Ordena os vetores lidos e conta.
+	for(i = 0; i < root.instances_count; i++)
 	{
-		linear_sort(instances[i].elements, instances[i].elements_count);
-		/*
-		for(j = 0; j < length(instances[i]); j++)
-		{
-			printf("%d", instances[i][j]);
-		}
-		*/
+		root.instances[i].sort_count = linear_sort(root.instances[i].elements, root.instances[i].elements_count);
+	}
+
+	// Imprime aqui o número de operações de cada sort [ordenação].
+	for(i = 0; i < root.instances_count; i++)
+	{
+		printf("%d", root.instances[i].sort_count);
 	}
 }
